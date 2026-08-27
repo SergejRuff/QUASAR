99a6d1c310f139fba6689d45e7f68e039b4495c5
Sergej Ruff <143480491+SergejRuff@users.noreply.github.com>
2026-08-27 11:38:14 +0200

    site

--- data-raw/update_md.R
+++ data-raw/update_md.R
@@ -1,1 +1,29 @@
 rmarkdown::render("README.Rmd", output_format = "github_document")
+
+
+# vignette
+usethis::use_vignette("quasar-deconvolution", "Cell-type deconvolution with QUASAR")
+
+
+# render vignette
+rmarkdown::render("vignettes/quasar-deconvolution.Rmd")
+#devtools::install(build_vignettes = TRUE)
+vignette("quasar-deconvolution", package = "quasar")
+
+
+
+# built site
+unlink("docs", recursive = TRUE)
+pkgdown::build_site()
+
+
+pkgdown::preview_site()
+
+
+# Create github page
+#usethis::use_pkgdown_github_pages()
+
+
+usethis::use_github_pages()
+usethis::use_github_action("pkgdown")
+usethis::use_build_ignore("docs")
